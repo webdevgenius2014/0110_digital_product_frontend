d7069ef42fd6c4fde242143de54506140fc62839
talha telci <talhatelci@yandex.com>
2025-12-02 11:35:48 +0300

    fix: typescript warnings

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import { useState, useRef } from "react";
 import CollaboratePopup from "./ColloaboRate";
 
 interface HeroSectionProps {
--- src/components/WhatWeDo.tsx
+++ src/components/WhatWeDo.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useState, useRef } from "react";
+import React, { useRef } from "react";
 
 interface WhatWeDoProps {
   setTriggerGlow: (value: boolean) => void; // NEW
